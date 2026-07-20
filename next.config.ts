ef36242fd75dca5dae96a624a7a9cb77dc96727b
Mahmud Hasan Sabbir <mahmoodsabbir3087@gmail.com>
2026-07-20 16:28:25 +0600

    chore: configure Vercel deployment settings and update Next.js security headers and image patterns

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  allowedDevOrigins: ['*'],
+  reactStrictMode: true,
+  poweredByHeader: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
